e02a59e2f569dd89c15060971976cb90e0b0258f
hoang vo <hoangvo040601@gmail.com>
2023-10-09 11:09:52 +0700

    fix bug 04

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
-const path = require('path');
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.xlsx$/,
-            use: [
-                {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'public', // Thay đổi đường dẫn đầu ra tùy theo nhu cầu của bạn
-                        publicPath: path.resolve(__dirname, './modalUploadFile.js'), // Đường dẫn tới thư mục chứa component
-                    },
-                },
-            ],
-        });
-
-        return config;
-    },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
